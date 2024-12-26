d89d229edbb249763903f69003b7343522dd4e92
juan <juan2007@gmail.com>
2024-12-26 11:15:17 -0700

    sigo intentando ingresar posts desde production

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,5 +17,8 @@ app.use(cors({
 
 app.use("/bar-service-quotations", BarServicequotationRoutes);
 
-
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
 export default app;
