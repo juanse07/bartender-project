b32abb97b1a94246b90d0349173d581b937a1f91
juan <juan2007@gmail.com>
2024-12-28 00:36:24 -0700

    first deploy after socket conection worked

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,6 +21,14 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
     console.log("New client connected:", socket.id);
+
+    // Listen for messages
+    socket.on("message", (data) => {
+        console.log("Message received:", data);
+        // Echo back to the client
+        socket.emit("response", `Server received: ${data}`);
+    });
+
     socket.on("disconnect", () => {
         console.log("Client disconnected:", socket.id);
     });
