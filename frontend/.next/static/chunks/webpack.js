23e4dda2e3616af4f020d58f230fd3fbffb6c0ad
juan <juan2007@gmail.com>
2025-01-02 10:33:47 -0700

    prrobando imagenesd e background para lastssection

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -129,7 +129,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("d6048a9881c50a8a")
+/******/ 		__webpack_require__.h = () => ("c84b6d07de92c16e")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
