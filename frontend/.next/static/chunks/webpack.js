fd58f03d1d3f357e667b3c2d42e1035e535e5b38
juan <juan2007@gmail.com>
2025-01-02 16:51:11 -0700

    darkerbackground

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -129,7 +129,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("827826488058cc77")
+/******/ 		__webpack_require__.h = () => ("19fb7b26f99d1f65")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
