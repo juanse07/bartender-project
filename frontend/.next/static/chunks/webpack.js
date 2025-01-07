47d5f8d02f627e5eb9910a776a9bad25c0f1b151
juan <juan2007@gmail.com>
2025-01-06 17:45:26 -0700

    changed controller structure

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -129,7 +129,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("b7eb101fff7f9914")
+/******/ 		__webpack_require__.h = () => ("6f147ce77097a0f9")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
