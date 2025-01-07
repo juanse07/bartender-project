c63f1a36370d4a30fcbf7c980d3134fbcc0a344b
juan <juan2007@gmail.com>
2025-01-07 16:40:36 -0700

    controller for update created

--- backend/src/routes/BarServiceQuotation.ts
+++ backend/src/routes/BarServiceQuotation.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get("/",BarServiceQuotationController.getBarServiceQuotations);
 router.post("/",BarServiceQuotationController.createBarServiceQuotation);
+router.patch("/:id", BarServiceQuotationController.updateBarServiceQuotation);
 
 export default router;
