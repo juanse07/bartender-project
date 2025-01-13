3220b3e4becdc6ecaf8068900574783a12a1adb4
juan <juan2007@gmail.com>
2025-01-13 09:09:56 -0700

    scroll to top fixed

--- frontend/src/pages/estimate-event.tsx
+++ frontend/src/pages/estimate-event.tsx
@@ -1,10 +1,27 @@
 import EventQuestionnaire from '@/components/forms/EventQuestionnaire';
-import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function BookEventPage() {
-  useEffect(() => {
-    document.title = 'Estimate Event | Eventable';
-    window.scrollTo(0, 0);
-  }, []);
-  return <EventQuestionnaire />;
+  const router = useRouter();
+
+  // useEffect(() => {
+  //   document.title = 'Estimate Event | Eventable';
+    
+  //   // Properly configure router events
+  //   const handleRouteChange = () => {
+  //     window.scrollTo(0, 0);
+  //   };
+
+  //   router.events.on('routeChangeComplete', handleRouteChange);
+
+  //   return () => {
+  //     router.events.off('routeChangeComplete', handleRouteChange);
+  //   };
+  // }, [router.events]);
+
+  return (
+    <div>
+      <EventQuestionnaire />
+    </div>
+  );
 }
