c7973c70ee0a639348f5f11aca990b92dd618c18
juan <juan2007@gmail.com>
2025-01-13 10:19:17 -0700

    scroll beh smooth added wrapper

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -129,7 +129,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("62f399f0f723280f")
+/******/ 		__webpack_require__.h = () => ("60243fe4bec6608a")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
