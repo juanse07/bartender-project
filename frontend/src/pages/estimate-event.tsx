081ef50f721294fae837c7188099b57bd9eb0de4
juan <juan2007@gmail.com>
2025-01-14 20:01:22 -0700

    error with safari persists

--- frontend/src/pages/estimate-event.tsx
+++ frontend/src/pages/estimate-event.tsx
@@ -1,23 +1,9 @@
 import EventQuestionnaire from '@/components/forms/EventQuestionnaire';
 import { useRouter } from 'next/router';
-
+import { NextPage } from 'next';
 export default function BookEventPage() {
   const router = useRouter();
 
-  // useEffect(() => {
-  //   document.title = 'Estimate Event | Eventable';
-    
-  //   // Properly configure router events
-  //   const handleRouteChange = () => {
-  //     window.scrollTo(0, 0);
-  //   };
-
-  //   router.events.on('routeChangeComplete', handleRouteChange);
-
-  //   return () => {
-  //     router.events.off('routeChangeComplete', handleRouteChange);
-  //   };
-  // }, [router.events]);
 
   return (
     <div>
