89b4d085e5caa2f100863c1696d8214944fa7806
juan <juan2007@gmail.com>
2025-02-03 10:56:32 -0700

    now fixed again enviroment variables mismatch env.ts

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -8,8 +8,9 @@ const env = cleanEnv(process.env, {
     SERVER_URL: str(),
     APNS_KEY_ID: str(),
     APNS_TEAM_ID: str(),
-    APNS_TOPIC: str(),
-    APNS_AUTH_KEY: str(),
+    APNS_ENV: str(),
+    APNS_BUNDLE_ID: str(),
+    APNS_AUTH_KEY_PATH: str(),
 
 });
 
